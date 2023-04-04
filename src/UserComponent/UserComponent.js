fce7c403d1d105be8ea27542949c0eeb8884c324
Nazik <nazzikmart@gmail.com>
2023-04-04 20:38:06 +0300

    new component adn new scripts userhats

--- src/UserComponent/UserComponent.js
+++ src/UserComponent/UserComponent.js
@@ -3,10 +3,14 @@ import "../App.css";
 import { useState } from "react";
 import InfoChatHeader from "../InfoChatHeader/InfoChatHeader";
 import MessageUserChatAdd from "../MessageUserChatAdd/MessageUserChatAdd";
+import AddUserForm from "../AddUserForm/AddUserForm";
 
 export default function UserComponent(props) {
   const [state, setState] = useState(false);
   const [infoname, setInfoName] = useState();
+
+  const [state1, setState1] = useState("");
+  const [toogle, setToogle] = useState(true);
   function addChatUser() {
     setState(true);
   }
@@ -16,6 +20,14 @@ export default function UserComponent(props) {
       setInfoName(card.querySelector("h4").textContent);
     });
   });
+
+  function addForm() {
+    setState1(<AddUserForm closeAddFormUser={closeForm} />);
+  }
+
+  function closeForm() {
+    setState1("");
+  }
 
   const users = [
     { id: 1, name: "John Doe", message: "Hello, how are you doing today?" },
@@ -41,7 +53,36 @@ export default function UserComponent(props) {
             </div>
           );
         })}
-        <p className="noMessageUser">Почніть чат</p>
+        <div className="noMessageUser">
+          <svg
+            style={{
+              // border: "2px solid black",
+              padding: " 20px",
+              // borderRadius: "50%",
+              position: "relative",
+              marginLeft: "60px",
+              marginTop: "-30px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="106"
+            height="106"
+            fill="currentColor"
+            class="bi bi-send"
+            viewBox="0 0 16 16"
+          >
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+          </svg>
+          <p className="messUserNoChats">Ваше повідомлення</p>
+          <span className="messTwoUserNoChats">
+            Надсилайте приватні світлини та повідомлення другу або групі.
+          </span>
+          <button className="btn-primary btnNewMessageUserNoChats">
+            Написати повідомлення
+          </button>
+        </div>
       </div>
     );
   } else {
@@ -63,7 +104,29 @@ export default function UserComponent(props) {
             </div>
           );
         })}
-        <InfoChatHeader name={<p className="nameUserChatUser">{infoname}</p>} />
+        <InfoChatHeader
+          name={
+            <p
+              className="nameUserChatUser"
+              style={{
+                display: "flex",
+              }}
+            >
+              {" "}
+              <div
+                className="chat__user-avatar1"
+                style={{
+                  position: "relative",
+                  width: "20px",
+                  height: "20px",
+                }}
+              >
+                <img src="https://via.placeholder.com/20" alt="User Avatar" />
+              </div>
+              <span className="newChatsName"> {infoname}</span>
+            </p>
+          }
+        />
         <MessageUserChatAdd />
       </div>
     );
